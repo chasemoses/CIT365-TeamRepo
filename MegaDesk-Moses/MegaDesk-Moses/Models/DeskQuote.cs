a6cdc56165fe3f92ae4feb8242027ed378f3fbd5
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-10-15 04:23:15 -0600

    Moved rushOrderPrices.txt to bin/debug as it wasn't letting me load otherwise.
    Also added in the code to read in the rushOrderPrices.txt file into a 2d array, and wrote conditionals to determine which value to pull out of the 2d array to give to the quote.

--- MegaDesk-Moses/MegaDesk-Moses/Models/DeskQuote.cs
+++ MegaDesk-Moses/MegaDesk-Moses/Models/DeskQuote.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using MegaDesk_Moses.Properties;
 
 namespace MegaDesk_Moses.Models
 {
@@ -20,6 +22,8 @@ namespace MegaDesk_Moses.Models
         public decimal TotalPrice => GetPricing().TotalCost;
 
         public bool IsComplete { get; set; }
+
+        const string FILENAME = "./rushOrderPrices.txt";
 
 
         public DeskQuote(Desk desk)
@@ -87,7 +91,7 @@ namespace MegaDesk_Moses.Models
             decimal materialCost = Desk.CalculateMaterialCost();
 
             // Calculate Expedited Cost based on area
-            decimal rushCost = CalculateRushDayCost(RushOrderDay, Desk.GetArea());
+            decimal rushCost = GetRushOrder(RushOrderDay, Desk.GetArea());
 
             PriceModel priceModel = new PriceModel(areaCost, drawerCost, materialCost,
                                                    rushCost);
@@ -95,47 +99,68 @@ namespace MegaDesk_Moses.Models
             return priceModel;
 
         }
-        private decimal CalculateRushDayCost(RushDay rushDay, int deskArea)
+        private decimal GetRushOrder(RushDay rushDay, int deskArea)
         {
-            switch(rushDay.Value)
+            // Create multi-dimensional array
+            decimal [,] rushOrderAmounts = new decimal [3, 3];
+
+            // If the file doesn't exist, don't continue.
+            if (!File.Exists(FILENAME))
             {
-                case 3:
-                    if (deskArea < 1000)
-                        return 60m;
-
-                    else if (deskArea >= 1000 && deskArea <= 2000)
-                        return 70m;
-
-                    else if (deskArea > 2000)
-                        return 80m;
-                    break;
-
-                case 5:
-                    if (deskArea < 1000)
-                        return 40m;
-
-                    else if (deskArea >= 1000 && deskArea <= 2000)
-                        return 50m;
-
-                    else if (deskArea > 2000)
-                        return 60m;
-                    break;
-
-                case 7:
-                    if (deskArea < 1000)
-                        return 30m;
-
-                    else if (deskArea >= 1000 && deskArea <= 2000)
-                        return 35m;
-
-                    else if (deskArea > 2000)
-                        return 40m;
-                    break;
+                Console.WriteLine("File does not exist.");
+                return -1;
             }
 
+            try
+            {
+                // read values into a temp 1d array, then use nested for loop
+                // to populate the 2d array.
+                var tempArray = File.ReadAllLines(FILENAME);
+
+                int index = 0;
+
+                for (int i = 0; i <= 2; i++)
+                {
+                    for (int j = 0; j <= 2; j++)
+                    {
+                        rushOrderAmounts[i, j] = decimal.Parse(tempArray[index]);
+                        index++;
+                    }
+                }
+
+                // Get the row and column of what we need to get the value
+                // From the 2d array.
+                int row = FindRushOrderCost(rushDay);
+                int column = FindRushOrderCost(deskArea);
+
+                // Return final value as long as Rush Day is not normal 
+                if (row != -1)
+                    return rushOrderAmounts[row, column];
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Message: {e}");
+            }
+
+            // If we have gotten this far, then the rush day should be normal.
             return 0m;
+        }
 
+        private int FindRushOrderCost(int value)
+        {
+            if (value >= 288 && value < 1000 || value == 3)
+                return 0;
 
+            else if (value >= 1000 && value <= 2000 || value == 5)
+                return 1;
+
+            else if (value > 2000 || value == 7)
+                return 2;
+
+            // If we hit this point, then the rush day is normal.
+            return -1;
         }
     }
 }
