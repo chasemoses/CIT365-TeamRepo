802dcb6caaf0d171c9dc6f03bd35e9e18a75e8cb
allan <marinaallan92@gmail.com>
2022-12-10 22:13:05 -0700

    Updated Model: SacramentProgram.cs to involve entry validation.

--- SMDesigner/Models/SacramentProgram.cs
+++ SMDesigner/Models/SacramentProgram.cs
@@ -8,26 +8,63 @@ namespace SMDesigner.Models
     public class SacramentProgram
     {
         public int ID { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Conductor")]
+        [StringLength(30, MinimumLength =5)]
+        [Required]
         public string ConductorL { get; set; }
-        [Display(Name = "Opening Song")]        
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
+        [Display(Name = "Opening Song")]
+        [StringLength(45, MinimumLength = 5)]
+        [Required]
         public string OpenSong { get; set; }
+
         [Display(Name = "Date")]
         [DataType(DataType.Date)]
         public DateTime ProgramDate { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Opening Prayer")]
+        [StringLength(30, MinimumLength = 5)]
+        [Required]
         public string OpenPrayer { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Sacrament Song")]
+        [StringLength(45, MinimumLength = 5)]
+        [Required]
         public string SacramentSong { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Speaker")]
+        [StringLength(30, MinimumLength = 5)]
+        [Required]
         public string SpeakerFullName { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Subject")]
+        [StringLength(30, MinimumLength = 5)]
+        [Required]
         public string Subject { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Intermediate Number")]
+        [StringLength(45, MinimumLength = 5)]
+        [Required]
         public string IntermedNum { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Closing Song")]
+        [StringLength(45, MinimumLength = 5)]
+        [Required]
         public string CloseSong { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$")]
         [Display(Name = "Closing Prayer")]
+        [StringLength(30, MinimumLength = 5)]
+        [Required]
         public string ClosePrayer { get; set; }
 
     }
