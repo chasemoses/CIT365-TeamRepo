f936de10e3360018a88324ac1a2a056c3b8ccab7
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-12-13 15:05:04 -0700

    Updated DbInitializer.cs, and added personalizing text for the SacramentPrograms/Index.cshtml

--- SMDesigner/Data/DbInitializer.cs
+++ SMDesigner/Data/DbInitializer.cs
@@ -19,9 +19,9 @@ namespace SMDesigner.Data
 
             var sacramentprograms = new SacramentProgram[]
             {
-            new SacramentProgram{ConductorL="Guss Rat", OpenSong="The Morning Breaks", ProgramDate=DateTime.Parse("2005-09-01"), OpenPrayer="Jasper Dog", SacramentSong="Child of God", Speakers={"Chloe Cat", "Oreo Dog" }, Subject="Jesus", IntermedNum="Piano", CloseSong="Choose the Right", ClosePrayer="Luna Dog"},
-            new SacramentProgram{ConductorL="Ragnar Lizard", OpenSong="Praise to the Man", ProgramDate=DateTime.Parse("2005-09-01"), OpenPrayer="Rambo Dog", SacramentSong="I Love to See the Temple", Speakers={"Ringo Dog", "Sasha Cat" }, Subject="Temples", IntermedNum="", CloseSong="Come Thou Font", ClosePrayer="Vanessa Cat"},
-            new SacramentProgram{ConductorL="Lilly Dog", OpenSong="Lead, Kindly Light", ProgramDate=DateTime.Parse("2005-09-01"), OpenPrayer="Austin Dog", SacramentSong="The Spirit of God", Speakers={"Sophie Rat", "Perry Platapus" }, Subject="Prophets", IntermedNum="Violin", CloseSong="Love One Another", ClosePrayer="Harry Frog"},
+            new SacramentProgram{ConductorL="Guss Rat", OpenSong="The Morning Breaks", ProgramDate=DateTime.Parse("2010-09-01"), OpenPrayer="Jasper Dog", SacramentSong="Child of God", Speakers={"Chloe Cat", "Oreo Dog" }, Subject="Jesus", IntermedNum="Piano", CloseSong="Choose the Right", ClosePrayer="Luna Dog"},
+            new SacramentProgram{ConductorL="Ragnar Lizard", OpenSong="Praise to the Man", ProgramDate=DateTime.Parse("2015-09-01"), OpenPrayer="Rambo Dog", SacramentSong="I Love to See the Temple", Speakers={"Ringo Dog", "Sasha Cat" }, Subject="Temples", IntermedNum="", CloseSong="Come Thou Font", ClosePrayer="Vanessa Cat"},
+            new SacramentProgram{ConductorL="Lilly Dog", OpenSong="Lead, Kindly Light", ProgramDate=DateTime.Parse("2015-12-12"), OpenPrayer="Austin Dog", SacramentSong="The Spirit of God", Speakers={"Sophie Rat", "Perry Platapus" }, Subject="Prophets", IntermedNum="Violin", CloseSong="Love One Another", ClosePrayer="Harry Frog"},
 
             };
             foreach (SacramentProgram s in sacramentprograms)
